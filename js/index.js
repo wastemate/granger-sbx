cd087a67586a8b0047f651b1d016f652e9e1b2a3
Jed Dawson <jed@edisoncode.com>
2015-10-06 23:41:21 -0700

    Built WasteMate.js from commit f6f1e93 on branch master

---
+++ js/index.js
@@ -0,0 +1,31 @@
+var wma_viewModel;
+$(document).ready(function () {
+  //6RvJ4eHRqgagOYjVYBrF9IzBc4czbXt7NplOqmDU
+  //6YJjl9Tlu9gml6IR0YfXrOIkY9SxqCfP2bshQELI
+  var appKey = '6RvJ4eHRqgagOYjVYBrF9IzBc4czbXt7NplOqmDU';
+  //xntysKgobBDGPDz1LOHsPwByZ6DkRS8mY3I9sfxK
+  //DEIXEICevT5qkR1zQxvj8PVHrvWu4XPKN2QUhhmL
+  var jsKey = 'xntysKgobBDGPDz1LOHsPwByZ6DkRS8mY3I9sfxK';
+  wma_viewModel = new viewModel();
+  ko.applyBindings(wma_viewModel);
+  wastemate.initialize(appKey, jsKey).then(function (categories) {
+    //make the app visible
+    wma_viewModel.shouldShowWMA(true);
+    //make search visible!
+    wma_viewModel.show('search');
+    //After the search is made visible, hookup live address library to the UI input.
+    // wireUpLiveAddress('#street_address', '4160067421270775959');
+    setupLiveAddressGoogle(wma_viewModel);
+    //Add each of the categories to the UI
+    $.each(categories, function (index, category) {
+      wma_viewModel.categories.push(category);
+    });
+    bindViewFormatters();
+  }, function (err) {
+    //something must not be right!
+    console.log(err);
+    alert('Unable to connect. Double check the settings!');
+    $('#loading').fadeOut();
+    $('#initialize').fadeIn();
+  });
+});
